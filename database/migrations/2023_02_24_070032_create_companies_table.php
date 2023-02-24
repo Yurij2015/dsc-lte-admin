7a9035066c96ed6125a9bf51ea758059fb8d1e0e
Yurij M <mokruy9999@gmail.com>
2023-02-24 19:51:08 +0100

    companies and customers migrations

--- database/migrations/2023_02_24_070032_create_companies_table.php
+++ database/migrations/2023_02_24_070032_create_companies_table.php
@@ -4,16 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('companies', function (Blueprint $table) {
+        Schema::create('companies', static function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('email', 80)->nullable();
+            $table->string('phone', 25)->nullable();
+            $table->string('address', 150)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('region', 50)->nullable();
+            $table->string('country', 2)->nullable()->default('PL');
+            $table->string('postal_code', 25)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2023_02_24_175348_create_customers_table.php
+++ database/migrations/2023_02_24_175348_create_customers_table.php
@@ -4,16 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('customers', static function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('company_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->string('first_name', 25);
+            $table->string('last_name', 25);
+            $table->string('email', 50)->nullable();
+            $table->string('phone', 25)->nullable();
+            $table->string('address', 150)->nullable();
+            $table->string('city', 50)->nullable();
+            $table->string('region', 50)->nullable();
+            $table->string('country', 2)->nullable()->default('PL');
+            $table->string('postal_code', 25)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
